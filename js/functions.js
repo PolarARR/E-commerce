a385de5316d8e144fcd64cbd6512c177b16a3091
PolarARR <angel-janus@hotmail.com>
2025-04-06 04:23:53 -0600

    first criterion

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,33 @@
 const header = document.querySelector("header");
 const cart = document.querySelector(".cart");
+const menu = document.querySelector(".menu");
 const cartIcon = header.lastElementChild;
+const menuIcon = header.firstElementChild;
+const quitIcon = cartIcon.querySelector("img");
+const closeIcon = menu.lastElementChild;
+const iconRemove = document.querySelectorAll(".cart__remove");
 
 cartIcon.addEventListener("click", () => {
-    const quitIcon = cartIcon.querySelector("img");
-    cart.classList.toggle("show");
+    cart.classList.toggle("showCart");
     
-    if (cart.classList.contains("show")) {
+    if (cart.classList.contains("showCart")) {
         quitIcon.setAttribute("src", "img/x-icon.svg");
     } else {
         quitIcon.setAttribute("src", "img/shopping-cart-icon.svg");
     }
 });
 
-const iconRemove = document.querySelectorAll(".cart__remove");
-
 iconRemove.forEach((icon) => {
     icon.addEventListener("click", () => {
         const iconParent = icon.parentElement;
         iconParent.remove();
     });
+});
+
+menuIcon.addEventListener("click", () => {
+    menu.classList.toggle("showMenu");
+});
+
+closeIcon.addEventListener("click", () => {
+    menu.classList.toggle("showMenu");
 });
